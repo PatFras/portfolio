994f4dcaf5e7deea7f66f98394120d30b167e2ac
Patricio Frascaroli <pmfrascaroli@gmail.com>
2024-02-06 19:21:22 -0300

    Stack section responsive + fix theme selector

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,9 @@
 import { defineConfig } from 'astro/config';
-
 import tailwind from "@astrojs/tailwind";
+
+import robotsTxt from "astro-robots-txt";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()]
+  integrations: [tailwind(), robotsTxt()]
 });
